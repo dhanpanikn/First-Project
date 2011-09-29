3daf6cd937d6295a23311cd691ef60bf4897c45b
Dhandapani <dhanpanikn@gmail.com>
2011-09-29 13:33:41 +0530

    Second commit

---
+++ Exercise3.py
@@ -0,0 +1,13 @@
+# Testing for Comments
+print " hello"
+print 'how r u'
+print "maths", 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print " Hi", 20/2 + 4
+print "maths", 4 % 2 - 1 / 4 + 6
+print "maths", 3 + 2 + 1 - 5 + 4 % 2 - 8 / 4 
+emp = 15
+print "In Savant there are", emp, "employees are working"
+age = 32
+height = 162
+weight = 70
+print "if i add %d, %d, and %d and I get %d" % (age, height, weight, age+height+weight)
